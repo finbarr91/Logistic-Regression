34061bc7fd43b7c01db9854b64ebe42cd9f02c34
Obierefu Finbarr Chukwuka <57522378+finbarr91@users.noreply.github.com>
2022-01-30 22:29:01 -0700

    complete project

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,1 +1,216 @@
+import numpy as np
+import scipy as sp
+import matplotlib as mpl
+import matplotlib.cm as cm
+from matplotlib.colors import ListedColormap
+import matplotlib.pyplot as plt
+import pandas as pd
+pd.set_option('display.width', 500)
+pd.set_option('display.max_columns', 100)
+pd.set_option('display.notebook_repr_html', True)
+import seaborn as sns
+sns.set_style("whitegrid")
+sns.set_context("poster")
+import sklearn.model_selection
+
+c0=sns.color_palette()[0]
+c1=sns.color_palette()[1]
+c2=sns.color_palette()[2]
+
+cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
+cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
+cm = plt.cm.RdBu
+cm_bright = ListedColormap(['#FF0000', '#0000FF'])
+
+def points_plot(ax, Xtr, Xte, ytr, yte, clf, mesh=True, colorscale=cmap_light,
+                cdiscrete=cmap_bold, alpha=0.1, psize=10, zfunc=False, predicted=False):
+    h = .02
+    X=np.concatenate((Xtr, Xte))
+    x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+    y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+    xx, yy = np.meshgrid(np.linspace(x_min, x_max, 100),
+                         np.linspace(y_min, y_max, 100))
+
+    #plt.figure(figsize=(10,6))
+    if zfunc:
+        p0 = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 0]
+        p1 = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
+        Z=zfunc(p0, p1)
+    else:
+        Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    ZZ = Z.reshape(xx.shape)
+    if mesh:
+        plt.pcolormesh(xx, yy, ZZ, cmap=cmap_light, alpha=alpha, axes=ax)
+    if predicted:
+        showtr = clf.predict(Xtr)
+        showte = clf.predict(Xte)
+    else:
+        showtr = ytr
+        showte = yte
+    ax.scatter(Xtr[:, 0], Xtr[:, 1], c=showtr-1, cmap=cmap_bold,
+               s=psize, alpha=alpha,edgecolor="k")
+    # and testing points
+    ax.scatter(Xte[:, 0], Xte[:, 1], c=showte-1, cmap=cmap_bold,
+               alpha=alpha, marker="s", s=psize+10)
+    ax.set_xlim(xx.min(), xx.max())
+    ax.set_ylim(yy.min(), yy.max())
+    return ax,xx,yy
+
+def points_plot_prob(ax, Xtr, Xte, ytr, yte, clf, colorscale=cmap_light,
+                     cdiscrete=cmap_bold, ccolor=cm, psize=10, alpha=0.1):
+    ax,xx,yy = points_plot(ax, Xtr, Xte, ytr, yte, clf, mesh=False,
+                           colorscale=colorscale, cdiscrete=cdiscrete,
+                           psize=psize, alpha=alpha, predicted=True)
+    Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
+    Z = Z.reshape(xx.shape)
+    plt.contourf(xx, yy, Z, cmap=ccolor, alpha=.2, axes=ax)
+    cs2 = plt.contour(xx, yy, Z, cmap=ccolor, alpha=.6, axes=ax)
+    plt.clabel(cs2, fmt = '%2.1f', colors = 'k', fontsize=14)
+    return ax
+
+
+# A Motivating Example Using sklearn: Heights and Weights
+# We'll use a dataset of heights and weights of males and females to hone our understanding of classifiers. We load the data into a dataframe and plot it.
+
+dflog = pd.read_csv("01_heights_weights_genders.csv")
+print(dflog.head())
+# your turn
+_ = sns.lmplot(x='Height', y='Weight', data=dflog, fit_reg=False, hue='Gender')
+plt.show()
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+# Split the data into a training and test set.
+Xlr, Xtestlr, ylr, ytestlr = train_test_split(dflog[['Height','Weight']].values,
+                                              (dflog.Gender == "Male").values,random_state=5)
+
+clf = LogisticRegression()
+# Fit the model on the trainng data.
+clf.fit(Xlr, ylr)
+# Print the accuracy from the testing data.
+print(accuracy_score(clf.predict(Xtestlr), ytestlr))
+
+from sklearn.model_selection import KFold
+from sklearn.metrics import accuracy_score
+
+def cv_score(clf, x, y, score_func=accuracy_score):
+    result = 0
+    nfold = 5
+    for train, test in KFold(nfold).split(x): # split data into train/test groups, 5 times
+        clf.fit(x[train], y[train]) # fit
+        result += score_func(clf.predict(x[test]), y[test]) # evaluate score function on held-out data
+    return result / nfold # average
+
+
+# Below is an example of using the cv_score function for a basic logistic regression model without regularization.
+clf = LogisticRegression()
+score = cv_score(clf, Xlr, ylr)
+print(score)
+
+#the grid of parameters to search over
+Cs = [0.001, 0.1, 1, 10, 100]
+
+# your turn
+max_score = 0
+for C in Cs:
+    clf = LogisticRegression(C=C)
+    score = cv_score(clf, Xlr, ylr)
+    if score > max_score:
+        max_score = score
+        best_C = C
+print('Best C is {} and max score is {}'.format(best_C, max_score))
+
+# your turn
+clf = LogisticRegression(C=0.1)
+clf.fit(Xlr, ylr)
+print(accuracy_score(clf.predict(Xtestlr), ytestlr))
+
+"""There is basically no difference between this and the default model. I don't think there is a problem. Though in this particular case the default C works very well, it may not be the case in other situations. 
+Cross-validation and grid search is almost always recommended.
+"""
+
+# your turn
+from sklearn.model_selection import GridSearchCV
+param_grid = {'C':Cs}
+logreg_cv = GridSearchCV(LogisticRegression(), param_grid, cv = 5, scoring = "accuracy")
+logreg_cv.fit(Xlr, ylr)
+print('Best C is',logreg_cv.best_params_)
+print('Score =',accuracy_score(logreg_cv.predict(Xtestlr), ytestlr))
+
+"""
+Best C is {'C': 0.001}
+Score = 0.9256
+The best C is different, and the improvement of score is very miniscule. I don't know why the two methods yielded different best C, my guess is how the 5 fold splitting is done differently in the two methods.
+"""
+
+"""
+A Walkthrough of the Math Behind Logistic Regression
+Setting up Some Demo Code
+
+Let's first set some code up for classification that we will need for further discussion on the math. We first set up a function cv_optimize which takes a classifier clf, a grid of hyperparameters (such as a complexity parameter or regularization parameter) implemented as a dictionary parameters, a training set (as a samples x features array) Xtrain, and a set of labels ytrain. The code takes the traning set, splits it into n_folds parts, sets up n_folds folds, and carries out a cross-validation by splitting the training set into a training and validation section for each foldfor us. It prints the best value of the parameters, and retuens the best classifier to us.
+"""
+
+def cv_optimize(clf, parameters, Xtrain, ytrain, n_folds=5):
+    gs = sklearn.model_selection.GridSearchCV(clf, param_grid=parameters, cv=n_folds)
+    gs.fit(Xtrain, ytrain)
+    print("BEST PARAMS", gs.best_params_)
+    best = gs.best_estimator_
+    return best
+
+"""
+We then use this best classifier to fit the entire training set. This is done inside the do_classify function which takes a dataframe indf as input. It takes the columns in the list featurenames as the features used to train the classifier. The column targetname sets the target. The classification is done by setting those samples for which targetname has value target1val to the value 1, and all others to 0. We split the dataframe into 80% training and 20% testing by default, standardizing the dataset if desired. (Standardizing a data set involves scaling the data so that it has 0 mean and is described in units of its standard deviation. We then train the model on the training set using cross-validation. Having obtained the best classifier using cv_optimize, we retrain on the entire training set and calculate the training and testing accuracy, which we print. We return the split data and the trained classifier.
+"""
+
+from sklearn.model_selection import train_test_split
+
+def do_classify(clf, parameters, indf, featurenames, targetname, target1val, standardize=False, train_size=0.8):
+    subdf=indf[featurenames]
+    if standardize:
+        subdfstd=(subdf - subdf.mean())/subdf.std()
+    else:
+        subdfstd=subdf
+    X=subdfstd.values
+    y=(indf[targetname].values==target1val)*1
+    Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, train_size=train_size)
+    clf = cv_optimize(clf, parameters, Xtrain, ytrain)
+    clf=clf.fit(Xtrain, ytrain)
+    training_accuracy = clf.score(Xtrain, ytrain)
+    test_accuracy = clf.score(Xtest, ytest)
+    print("Accuracy on training data: {:0.2f}".format(training_accuracy))
+    print("Accuracy on test data:     {:0.2f}".format(test_accuracy))
+    return clf, Xtrain, ytrain, Xtest, ytest
+
+h = lambda z: 1. / (1 + np.exp(-z))
+zs=np.arange(-5, 5, 0.1)
+plt.plot(zs, h(zs), alpha=0.5)
+plt.show()
+
+print(dflog.head())
+
+clf_l, Xtrain_l, ytrain_l, Xtest_l, ytest_l  = do_classify(LogisticRegression(),
+                                                           {"C": [0.01, 0.1, 1, 10, 100]},
+                                                           dflog, ['Weight', 'Height'], 'Gender','Male')
+plt.figure()
+ax=plt.gca()
+points_plot(ax, Xtrain_l, Xtest_l, ytrain_l, ytest_l, clf_l, alpha=0.2)
+plt.show()
+
+clf_l.predict_proba(Xtest_l)
+
+
+# Discriminative vs Generative Classifier
+# Logistic regression is what is known as a discriminative classifier as we learn a soft boundary between/among classes. Another paradigm is the generative classifier where we learn the distribution of each class. For more examples of generative classifiers, look here.
+# Let us plot the probabilities obtained from predict_proba, overlayed on the samples with their true labels:
+
+plt.figure()
+ax = plt.gca()
+points_plot_prob(ax, Xtrain_l, Xtest_l, ytrain_l, ytest_l, clf_l, psize=20, alpha=0.1);
+
+
+plt.figure()
+ax = plt.gca()
+points_plot_prob(ax, Xtrain_l, Xtest_l, ytrain_l, ytest_l, clf_l, psize=20, alpha=0.1)
+plt.show()
 
